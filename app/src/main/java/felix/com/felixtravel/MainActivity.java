be064e5116e8cb44d1b86dab0747850031e49cef
felixlin <runafter400d@gmail.com>
2016-11-19 22:44:01 +0800

    add navigation view

--- app/src/main/java/felix/com/felixtravel/MainActivity.java
+++ app/src/main/java/felix/com/felixtravel/MainActivity.java
@@ -2,30 +2,34 @@ package felix.com.felixtravel;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String NAV_ITEM_ID = "nav_index";
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+    private TextView mContentView;
+    private NavigationView mNavigationView;
+    private int navItemId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        initView();
+        setListeners(savedInstanceState);
     }
 
     @Override
@@ -49,4 +53,66 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(NAV_ITEM_ID, navItemId);
+    }
+
+
+    private void navigateTo(MenuItem menuItem){
+        mContentView.setText(menuItem.getTitle());
+
+        navItemId = menuItem.getItemId();
+        menuItem.setChecked(true);
+    }
+
+    private void initView(){
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mContentView = (TextView) findViewById(R.id.content_view);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+    }
+
+    private void setListeners(Bundle savedInstanceState){
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Toast.makeText(MainActivity.this, menuItem.getTitle() + " pressed", Toast.LENGTH_LONG).show();
+                navigateTo(menuItem);
+
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.openDrawer, R.string.closeDrawer){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        if(null != savedInstanceState){
+            navItemId = savedInstanceState.getInt(NAV_ITEM_ID, R.id.navigation_item_1);
+        }
+        else{
+            navItemId = R.id.navigation_item_1;
+        }
+
+        navigateTo(mNavigationView.getMenu().findItem(navItemId));
+    }
+
 }
